a30e950fb81694b907cce23fb18f239d6d0d18ec
plocplic <62767033+plocplic@users.noreply.github.com>
2020-05-04 19:06:36 +0200

    Update server1.js

--- nodejs/server1.js
+++ nodejs/server1.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write('<html lang="fr">'+
+	res.write('<html lang="fr">'+                    //j'ai collé mon code html
 	<link rel="stylesheet" href="robot.css">'+
    	<script src="robot.js"></script>'+
 	<head>'+
